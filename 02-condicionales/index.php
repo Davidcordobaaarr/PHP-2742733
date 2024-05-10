fe1037d8684048918c1f75398a0337dfe2db4e56
DaniCode212 <danielperezbedoya382@gmail.com>
2024-05-10 16:06:22 -0500

    feat: sintaxis-variables

---
+++ 02-condicionales/index.php
@@ -0,0 +1,58 @@
+<?php
+
+echo 'ejecutando condicionales...';
+
+
+$articulos = "30";
+
+
+
+if($articulos < 12) {
+
+    echo "caja rapida";
+ 
+} else if($articulos > 12){
+
+    echo "caja normal";
+    
+}
+
+
+
+echo "<br> <br>";
+
+
+
+$edad = '10';
+
+if($edad >= 18){
+    echo 'SI PUEDE VOTAR';
+}
+
+else if($edad < 18){
+    echo 'NO PUEDE VOTAR';
+}
+
+
+echo "<br> <br>";
+
+$nombre = mario;
+
+$edad = 18; 
+
+if ($edad >= 18){
+    echo 'Puede ingresar a la discoteca';
+}
+
+else if($edad < 18){
+    echo 'No puedes ingresar a la dicoteca';
+}
+
+$david = 12;
+
+?>
+
+
+
+
+
