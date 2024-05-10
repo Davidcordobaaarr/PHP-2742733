2c24da0af31825e05e5fc529c1d7e43bdd502b55
DaniCode212 <danielperezbedoya382@gmail.com>
2024-05-10 17:20:18 -0500

    feat: sintaxis-condicionales

--- 02-condicionales/index.php
+++ 02-condicionales/index.php
@@ -30,36 +30,85 @@ if($edad >= 18){
 }
 
 else if($edad < 18){
+
     echo 'NO PUEDE VOTAR';
 }
 
 
 echo "<br> <br>";
 
-$nombre = mario;
 
-$edad = 18; 
+$nombre = "David";
+
+$edad = "30"; 
 
 if ($edad >= 18){
     echo 'Puede ingresar a la discoteca';
 }
 
-else if($edad < 18){
+else{
     echo 'No puedes ingresar a la dicoteca';
 }
 
-$articulos = "30";
+if($nombre == "mario" xor $nombre == "carlos") {
+    echo 'bienvenido puedes ingresar al VIP';
+}
 
 
 
-if($articulos < 12) {
+echo "<br> <br>";
 
-    echo "caja rapida";
- 
-} else if($articulos > 12){
+$estatura = "170";
 
-    echo "caja normal";
-    
+$velocidad = "20km";
+
+$edad = "13";
+
+if ($velocidad > 17 && $estatura > 150)
+
+{
+    echo "puede ingresar al equipo <br/>";
+}
+
+else {
+    echo "no puede ingresar <br/>";
+}
+
+if($velocidad > 17 && $estatura > 150 &&$edad>18){
+    echo "vas a las ligas mayores";
+}
+
+else{
+    echo "vas a las ligas menores";
+}
+
+echo "<br> <br>";
+
+$aire = "naranja";
+
+switch($aire){
+    case "verde";
+        echo "la calidad del aire es buena";
+    break;
+    case "amarillo";
+        echo "la calidad del aire es moderada";
+    break;
+    case "naranja";
+        echo "la calidad del aire no es saludable para grupos sensibles";
+    break;
+    case "rojo";
+        echo "la calidad del aire no es saludable";
+    break;
+    case "púrpura";
+        echo "la calidad dle aire es muy poco saludable";
+    break;
+    case "marrón";
+        echo "la calidad del aire es peligrosa";
+    break;
+    default;
+        echo "el color no es valido";
+    break;
+
 }
 
 ?>
