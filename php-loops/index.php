5c16de08c9315dc2d3b455ff6abe74963600b5ff
DaniCode212 <danielperezbedoya382@gmail.com>
2024-05-14 16:00:07 -0500

    feat: loop-php

---
+++ php-loops/index.php
@@ -0,0 +1,32 @@
+<?php
+
+//While  ciclo que imprime la tabla del 5
+
+$i = 0;
+while ($i < 50) {
+    $i+=5;
+    echo $i;
+echo"<br>";
+}
+
+$i = 1;
+while ($i <= 10) {
+  echo "5 x $i = " . (5 * $i) . "<br>";
+  $i++;
+}
+
+//for imprime una lista de nombres 
+
+$i = 0;
+for ($i = 3; $i <= 10; $i++) {
+    if ($i % 3 == 0) {
+        echo 'multiplo de 3';
+    }
+}
+
+
+
+// foreach 
+
+
+?>
