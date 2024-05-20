6e834d271348119a358a4c963bb452b7cc16ffde
Davidcordobaaarr <davidcordoba036@gmail.com>
2024-05-20 15:54:34 -0500

    feat: avance funciones

---
+++ 05-include-require/componentes/footer.php
@@ -0,0 +1,8 @@
+<footer class"bg-dark">
+    <div class"container"></div>
+        <div class"row">
+            <p class="mb-0 text-center">Todos los derechos reservador
+                multimedia 2024 😌
+            </p>
+        </div>
+</footer>
