5da0a97d345802792f2acdec2f19dc7f03989dcb
Davidcordobaaarr <davidcordoba036@gmail.com>
2024-05-21 16:07:15 -0500

    feat: changes SQL

---
+++ 07-SQL/index.php
@@ -0,0 +1,35 @@
+<?php
+
+    //DOCUMENTACION: SQL w3school
+
+    //1. SQL SQL select
+    //SELECT materia, FROM profesores 
+    //
+
+    //2. SQL select distinc 
+    //SELECT DISTINCT`ID`, `Nombre`, `edad`,`Documento` FROM `profesores`;
+    //solo extrae datos unicos sin repetirlos 
+
+    //3. SQL select where
+    //SELECT * FROM `profesores` WHERE 1;
+    //captura valores unicos de una columna
+
+    //3. SQL select orederBY
+    //continuo mañana en clase :)
+    //
+
+    
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
