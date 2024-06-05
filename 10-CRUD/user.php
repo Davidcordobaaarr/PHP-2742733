cca6716287a293e7159975a301ff1ba383b8140e
Davidcordobaaarr <davidcordoba036@gmail.com>
2024-06-05 16:19:27 -0500

    feat: crud avance

---
+++ 10-CRUD/user.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>user page</title>
+    <link rel="stylesheet" href="user.css">
+</head>
+
+<body>
+
+    <?php if (isset($_SESSION['userRegister'])) : ?>
+
+        <div class="padre">
+
+            <div class="welcome">
+
+
+                <h1> Bienvenido 🎶 <?php echo $_SESSION['userRegister']; ?> </h1>
+                <a href="./cerrar.php">cerrar sesion </a>
+                <a href="./index.php">Home</a>
+
+            <?php else : ?>
+
+                <h1>no has iniciado sesion</h1>
+                <a href="./index.php">iniciar sesion</a>
+
+            <?php endif ?>
+
+            </div>
+
+        </div>
+
+</body>
+
+</html>
